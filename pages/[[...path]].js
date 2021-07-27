9e4f7176f728fe48d9d5c7d43cf4e547040c8b12
David Whittemore <github@xpollen8.com>
2021-07-26 17:55:05 -0700

    dw: another run at reading htdb files

--- pages/[[...path]].js
+++ pages/[[...path]].js
@@ -6,15 +6,38 @@ const { serverRuntimeConfig } = getConfig()
 
 let htdb;
 
+/*
 export async function getServerSideProps({ query: { path = [] } = {} }) {
 	if (!htdb) {
-		console.log("SET ROOT", process.cwd(), "OLD", serverRuntimeConfig.PROJECT_ROOT);
 		htdb = new HTDB(process.cwd(), 1);
 	}
 
 	return {
 		props: { body: await htdb.render(path.join('/')) }
 	}
+}
+*/
+
+export async function getStaticProps({ params: { path = [] } = {} }) {
+	console.log("ARGS", path, serverRuntimeConfig.PROJECT_ROOT);
+	//https://nextjs.org/docs/basic-features/data-fetching#getstaticpaths-static-generation
+	if (!htdb) {
+		htdb = new HTDB(serverRuntimeConfig.PROJECT_ROOT, 1);
+	}
+
+	return {
+		props: { body: await htdb.render(path.join('/')) }
+	}
+}
+
+export async function getStaticPaths() {
+	return {
+		paths: [
+			//{ params: { path: [ 'site', 'buh' ] } } // See the "paths" section below
+			{ params: { path: [ 'site' ] } } // See the "paths" section below
+		],
+		fallback: true
+	};
 }
 
 const App = ({ body }) => (
