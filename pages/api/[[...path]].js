f9e84d093a66e4503b215a4cae1cc1f2180185be
David Whittemore <github@xpollen8.com>
2021-07-27 18:05:33 -0700

    dw: buh1

--- pages/api/[[...path]].js
+++ pages/api/[[...path]].js
@@ -1,3 +1,4 @@
+import path from "path";
 import getConfig from 'next/config'
 import HTDB from 'HTDBjs';
 const { serverRuntimeConfig } = getConfig()
@@ -7,7 +8,13 @@ let htdb;
 const handler = async (req, res) => {
 	console.log("URL", req.url, __dirname);
 	if (!htdb) {
-		htdb = new HTDB(serverRuntimeConfig.PROJECT_ROOT, 1);
+		if (process.env.NODE_ENV === "production") {
+			console.log("PATH", path.join(process.cwd(), ".next/server/chunks"));
+			htdb = new HTDB(path.join(process.cwd(), ".next/server/chunks"), 1);
+		} else {
+			console.log("PATH", serverRuntimeConfig.PROJECT_ROOT);
+			htdb = new HTDB(serverRuntimeConfig.PROJECT_ROOT, 1);
+		}
 	}
 
 	const html = await htdb.render(req.url.replace('/api',''));
