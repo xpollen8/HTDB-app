238492f4e14b416b87eba5e480c681eaf7546025
David Whittemore <github@xpollen8.com>
2021-07-27 19:09:20 -0700

    dw: try reweites in next config

--- next.config.js
+++ next.config.js
@@ -8,6 +8,10 @@ module.exports = {
 	serverRuntimeConfig: {
 		PROJECT_ROOT: __dirname,
 	},
+	  "rewrites": [
+		    { "source": "/api/:path", "destination": "/api/$path" },
+				    { "source": "/:path", "destination": "/api/$path" }
+						  ],
 	target: "server",
 	future: {
 			webpack5: true,
