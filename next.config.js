09dc88591ec0a9d9c177d136c85c863eab7bbf32
xpollen8 <github@xpollen8.com>
2023-05-05 15:11:58 -0700

    dw: webpack debug

--- next.config.js
+++ next.config.js
@@ -1,44 +1,19 @@
-//const CopyPlugin = require("copy-webpack-plugin")
+const CopyPlugin = require("copy-webpack-plugin")
 
-//
-//module.exports = {
-//	publicRuntimeConfig: {
-//		// Will be available on both server and client
-//		staticFolder: '/htdb',
-//	},
-//	serverRuntimeConfig: {
-//		PROJECT_ROOT: __dirname,
-//	},
-//	//target: "server",
-//	//future: {
-//			//webpack5: true,
-//	//},
-////	webpack: function (config, { dev, isServer }) {
-////			// Fixes npm packages that depend on `fs` module
-////			//if (!isServer) {
-////					//config.resolve.fallback.fs = false
-////			//}
-////			// copy files you're interested in
-////			//if (!dev) {
-////					config.plugins.push(
-////							new CopyPlugin({
-////									patterns: [{ from: "./htdb/**", to: "." }],
-////							})
-////					)
-////			//}
-////
-////			return config
-////	},
-//}
-
-//const withConfig = require("next-config");
-//module.exports = withConfig({
-//  webpack(config, options) {
-//		config.plugins.push(
-//				new CopyPlugin({
-//						//patterns: [{ from: "htdb/**", to: "." }],
-//				})
-//		)
-//    return config;
-//  }
-//);
+module.exports = {
+	publicRuntimeConfig: {
+		// Will be available on both server and client
+		staticFolder: '/htdb',
+	},
+	serverRuntimeConfig: {
+		PROJECT_ROOT: __dirname,
+	},
+	webpack: function (config, { dev, isServer }) {
+		config.plugins.push(
+			new CopyPlugin({
+				patterns: [{ from: "./htdb/**", to: "server/chunks" }],
+			})
+		)
+		return config
+	},
+}
