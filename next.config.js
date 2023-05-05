cda6591255cd989a57768035cc80793966d1ff0e
xpollen8 <github@xpollen8.com>
2023-05-05 14:30:51 -0700

    dw: https://www.npmjs.com/package/next-config

--- next.config.js
+++ next.config.js
@@ -1,31 +1,44 @@
 const CopyPlugin = require("copy-webpack-plugin")
 
-module.exports = {
-	publicRuntimeConfig: {
-		// Will be available on both server and client
-		staticFolder: '/htdocs',
-	},
-	serverRuntimeConfig: {
-		PROJECT_ROOT: __dirname,
-	},
-	//target: "server",
-	//future: {
-			//webpack5: true,
-	//},
-	webpack: function (config, { dev, isServer }) {
-			// Fixes npm packages that depend on `fs` module
-			//if (!isServer) {
-					//config.resolve.fallback.fs = false
-			//}
-			// copy files you're interested in
-			//if (!dev) {
-					config.plugins.push(
-							new CopyPlugin({
-									patterns: [{ from: "./htdb/**", to: "." }],
-							})
-					)
-			//}
+//
+//module.exports = {
+//	publicRuntimeConfig: {
+//		// Will be available on both server and client
+//		staticFolder: '/htdb',
+//	},
+//	serverRuntimeConfig: {
+//		PROJECT_ROOT: __dirname,
+//	},
+//	//target: "server",
+//	//future: {
+//			//webpack5: true,
+//	//},
+////	webpack: function (config, { dev, isServer }) {
+////			// Fixes npm packages that depend on `fs` module
+////			//if (!isServer) {
+////					//config.resolve.fallback.fs = false
+////			//}
+////			// copy files you're interested in
+////			//if (!dev) {
+////					config.plugins.push(
+////							new CopyPlugin({
+////									patterns: [{ from: "./htdb/**", to: "." }],
+////							})
+////					)
+////			//}
+////
+////			return config
+////	},
+//}
 
-			return config
-	},
-}
+const withConfig = require("next-config");
+module.exports = withConfig({
+  webpack(config, options) {
+		config.plugins.push(
+				new CopyPlugin({
+						patterns: [{ from: "htdb/**", to: "." }],
+				})
+		)
+    return config;
+  }
+});
